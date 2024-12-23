09192dee58da399ca591020092cc28f7dd8126cf
Karthik123472 <karthikboora121@gmail.com>
2024-12-23 23:22:18 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MyConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double p = 2000;
+            
+            Console.WriteLine("sai rama");
+            Console.WriteLine(10);
+            Console.WriteLine(10.5);
+            Console.ReadLine();
+        }   
+    }
+}
+
+
+
+
+
